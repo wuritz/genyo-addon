b4451d7b8b2fc6854e7bc6e0e5fc90e94a228a1d
wuritz <ritzrichard51@gmail.com>
2025-06-21 12:25:55 +0200

    genyo jobb

--- src/main/java/com/genyo/addon/mixin/HudRendererAccessor.java
+++ src/main/java/com/genyo/addon/mixin/HudRendererAccessor.java
@@ -1,4 +1,13 @@
 package com.genyo.addon.mixin;
 
-public class HudRendererAccessor {
+import meteordevelopment.meteorclient.systems.hud.HudRenderer;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Accessor;
+
+@Mixin(HudRenderer.class)
+public interface HudRendererAccessor {
+
+    @Accessor(value = "INSTANCE", remap = false)
+    HudRenderer getHudRenderer();
+
 }
